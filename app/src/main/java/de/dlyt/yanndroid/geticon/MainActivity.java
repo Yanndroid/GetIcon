b6ba33134a8e7253bc2e884d6e09d274d0283875
yanndroid <yanndroid.dev@gmail.com>
2023-08-02 20:12:39 +0200

    app: export more icon variants project: bump dependencies

--- app/src/main/java/de/dlyt/yanndroid/geticon/MainActivity.java
+++ app/src/main/java/de/dlyt/yanndroid/geticon/MainActivity.java
@@ -3,17 +3,15 @@ package de.dlyt.yanndroid.geticon;
 import android.annotation.SuppressLint;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.drawable.AdaptiveIconDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.text.TextUtils;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -25,23 +23,18 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
 import java.util.Comparator;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int ICON_SIZE = 512;
+
     private PackageManager packageManager;
     private List<ApplicationInfo> packages;
-
-    private int maskMenuId;
-    private int monoMenuId;
-    private boolean exportMask = true;
-    private boolean exportMono = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU;
-
-    private int size = 512;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +48,85 @@ public class MainActivity extends AppCompatActivity {
 
         list.setLayoutManager(new LinearLayoutManager(this));
         list.setAdapter(new ListAdapter());
+    }
+
+    private void laodAll(String packageName) {
+        try {
+            long timeStamp = System.currentTimeMillis();
+            Drawable base = packageManager.getApplicationIcon(packageName);
+
+            loadIconVariation(packageName, timeStamp, "default", base, false, false, false);
+            loadIconVariation(packageName, timeStamp, "default_mask", base, true, false, false);
+
+            loadIconVariation(packageName, timeStamp, "mono", base, false, true, false);
+            loadIconVariation(packageName, timeStamp, "mono_mask", base, true, true, false);
+
+            loadIconVariation(packageName, timeStamp, "mono_dark", base, false, true, true);
+            loadIconVariation(packageName, timeStamp, "mono_dark_mask", base, true, true, true);
+
+            Toast.makeText(this, packageName, Toast.LENGTH_SHORT).show();
+        } catch (PackageManager.NameNotFoundException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    private void loadIconVariation(String packageName, long timeStamp, String suffix, Drawable base, boolean mask, boolean mono, boolean darkMode) {
+        Bitmap icon = buildIcon(base, ICON_SIZE, mask, mono, darkMode);
+        try {
+            saveBitmap(icon, packageName, timeStamp, suffix);
+        } catch (IOException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    private Bitmap buildIcon(Drawable drawable, int size, boolean mask, boolean mono, boolean darkMode) {
+        if (drawable instanceof AdaptiveIconDrawable) {
+            AdaptiveIconDrawable adaptiveDrawable = (AdaptiveIconDrawable) drawable;
+            adaptiveDrawable.setBounds(0, 0, size, size);
+
+            Drawable background = adaptiveDrawable.getBackground().mutate();
+            Drawable foreground = adaptiveDrawable.getForeground().mutate();
+
+            if (mono && Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                Drawable monochrome = adaptiveDrawable.getMonochrome();
+                if (monochrome != null) foreground = monochrome.mutate();
+            }
+
+            if (mono) {
+                int bg_tint = Color.parseColor(darkMode ? "#003549" : "#4BB6E8");
+                int fg_tint = darkMode ? Color.parseColor("#E0F3FF") : Color.WHITE;
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    bg_tint = getColor(darkMode ? android.R.color.system_accent1_800 : android.R.color.system_accent1_300);
+                    fg_tint = darkMode ? getColor(android.R.color.system_accent1_50) : Color.WHITE;
+                }
+
+                background.setTint(bg_tint);
+                foreground.setTint(fg_tint);
+            }
+
+            Bitmap output = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(output);
+            if (mask) canvas.clipPath(adaptiveDrawable.getIconMask());
+            background.draw(canvas);
+            foreground.draw(canvas);
+            return output;
+        }
+
+        Bitmap output = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+        drawable.draw(canvas);
+        return output;
+    }
+
+    @SuppressLint("DefaultLocale")
+    private void saveBitmap(Bitmap bitmap, String packageName, long timeStamp, String suffix) throws IOException {
+        String fileName = String.format("%s_%d_%s.png", packageName, timeStamp, suffix);
+        OutputStream os = Files.newOutputStream(new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), fileName).toPath());
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, os);
+        os.close();
     }
 
     private class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
@@ -73,7 +145,7 @@ public class MainActivity extends AppCompatActivity {
         public void onBindViewHolder(@NonNull ListAdapter.ViewHolder holder, int position) {
             String packageName = packages.get(position).packageName;
             ((TextView) holder.itemView).setText(packageName);
-            holder.itemView.setOnClickListener(v -> MainActivity.this.getIcons(packageName));
+            holder.itemView.setOnClickListener(v -> MainActivity.this.laodAll(packageName));
         }
 
         @Override
@@ -88,95 +160,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void getIcons(String packageName) {
-        try {
-            long timeStamp = System.currentTimeMillis();
-            Drawable icon = packageManager.getApplicationIcon(packageName);
-
-            Bitmap def = buildIcon(icon, size, false, false);
-            if (def != null) {
-                saveBitmap(def, packageName, timeStamp, "default");
-                Toast.makeText(this, "default", Toast.LENGTH_SHORT).show();
-            }
-            if (exportMask) {
-                Bitmap mask = buildIcon(icon, size, false, true);
-                if (mask != null) {
-                    saveBitmap(mask, packageName, timeStamp, "mask");
-                    Toast.makeText(this, "mask", Toast.LENGTH_SHORT).show();
-                }
-            }
-            if (exportMono) {
-                Bitmap mono = buildIcon(icon, size, true, false);
-                if (mono != null) {
-                    saveBitmap(mono, packageName, timeStamp, "mono");
-                    Toast.makeText(this, "mono", Toast.LENGTH_SHORT).show();
-                }
-            }
-            if (exportMask && exportMono) {
-                Bitmap mask_mono = buildIcon(icon, size, true, true);
-                if (mask_mono != null) {
-                    saveBitmap(mask_mono, packageName, timeStamp, "mask_mono");
-                    Toast.makeText(this, "mask_mono", Toast.LENGTH_SHORT).show();
-                }
-            }
-        } catch (PackageManager.NameNotFoundException | IOException e) {
-            Toast.makeText(this, "Error" + e.getMessage(), Toast.LENGTH_SHORT).show();
-            e.printStackTrace();
-        }
-    }
-
-    @SuppressLint("InlinedApi")
-    private Bitmap buildIcon(Drawable drawable, int size, boolean mono, boolean mask) {
-        if (mono && android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.TIRAMISU)
-            return null;
-        if (drawable instanceof AdaptiveIconDrawable) {
-            AdaptiveIconDrawable adaptiveDrawable = (AdaptiveIconDrawable) drawable;
-            adaptiveDrawable.setBounds(0, 0, size, size);
-
-            Drawable background = adaptiveDrawable.getBackground().mutate();
-            Drawable foreground = mono ? adaptiveDrawable.getMonochrome() : adaptiveDrawable.getForeground().mutate();
-            if (background == null || foreground == null) return null;
-
-            if (mono) {
-                boolean darkMode = isDarkMode();
-                background.setTint(getColor(darkMode ? android.R.color.system_accent1_800 : android.R.color.system_accent1_300));
-                foreground.setTint(darkMode ? getColor(android.R.color.system_accent1_50) : Color.WHITE);
-            }
-
-            Bitmap output = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
-            Canvas canvas = new Canvas(output);
-            if (mask) canvas.clipPath(adaptiveDrawable.getIconMask());
-            background.draw(canvas);
-            foreground.draw(canvas);
-            return output;
-        }
-        return null;
-    }
-
-    private boolean isDarkMode() {
-        return (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES;
-    }
-
-    @SuppressLint("DefaultLocale")
-    private void saveBitmap(Bitmap bitmap, String packageName, long timeStamp, String suffix) throws IOException {
-        String fileName = String.format("%s_%d_%s.png", packageName, timeStamp, suffix);
-        OutputStream os = new FileOutputStream(new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), fileName));
-        bitmap.compress(Bitmap.CompressFormat.PNG, 0, os);
-        os.close();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        maskMenuId = menu.add("Export masked").setCheckable(true).setChecked(exportMask).getItemId();
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU)
-            monoMenuId = menu.add("Export monochrome").setCheckable(true).setChecked(exportMono).getItemId();
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == maskMenuId) item.setChecked(exportMask = !exportMask);
-        if (item.getItemId() == monoMenuId) item.setChecked(exportMono = !exportMono);
-        return true;
-    }
 }
